e2aeebd773f5aee30523add1d3cac32aa6735d49
kevinkorte <kevinkorte15@gmail.com>
2017-02-15 23:47:35 -0800

    need to update stripe quantity when adding new seats

--- client/accounts/ui/invite.js
+++ client/accounts/ui/invite.js
@@ -10,16 +10,20 @@ Template.invite.helpers({
   },
   numUsers: function() {
     let user = Meteor.users.findOne(Meteor.userId());
-    let organization = Organizations.findOne(user.organizationId);
-    if (organization) {
-      return organization.quantityUsed;
+    if (user) {
+      let organization = Organizations.findOne(user.organizationId);
+      if (organization) {
+        return organization.quantityUsed;
+      }
     }
   },
   maxUsers: function() {
     let user = Meteor.users.findOne(Meteor.userId());
-    let organization = Organizations.findOne(user.organizationId);
-    if (organization) {
-      return organization.quantity;
+    if (user) {
+      let organization = Organizations.findOne(user.organizationId);
+      if (organization) {
+        return organization.quantity;
+      }
     }
   }
 });
@@ -36,5 +40,26 @@ Template.invite.events({
         console.log(response);
       }
     })
+  },
+  'click .add-more-users'(event) {
+    event.preventDefault();
+    $('.add-more-users').addClass('submit-new-users').removeClass('add-more-users');
+    let text = $('.submit-new-users').data('text');
+    $('.submit-new-users').text(text);
+    $('.submit-new-users').after("<input type='number' name='additionalSeats'>");
+  },
+  'submit .newSeats'(event) {
+    console.log('submit new users');
+    event.preventDefault();
+    const target = event.target;
+    const additionalSeats = target.additionalSeats.value;
+    console.log(additionalSeats);
+    Meteor.call('addAdditionalSeats', additionalSeats, function(error, response) {
+      if (error) {
+        console.log(error.reason);
+      } else {
+        console.log(response);
+      }
+    });
   }
 })
--- server/methods/email/methods.js
+++ server/methods/email/methods.js
@@ -1,9 +1,56 @@
+const Future = Npm.require('fibers/future');
+
+const secret = Meteor.settings.private.stripe.testSecretKey;
+const Stripe = StripeAPI(secret);
+
 Meteor.methods({
   inviteUser: function(email) {
     check(email, String);
-    let user = Accounts.createUser({email: email});
+    let user = Meteor.users.findOne(Meteor.userId());
     if (user) {
-      Accounts.sendEnrollmentEmail(user)
+      let organization = Organizations.findOne(user.organizationId);
+      if (organization.quantityUsed < organization.quantity) {
+        Organizations.update(organization._id, {$inc: {quantityUsed: 1}}, function(error, result) {
+          if (error) {
+            console.log(error.reason);
+          } else {
+            let newUser = Accounts.createUser({email: email});
+            Meteor.users.update(newUser, {
+              $set: {organizationId: organization._id}
+            }, function(error, response) {
+              if (error) {
+                console.log(error);
+              } else {
+                console.log('sending enrollment email');
+                Accounts.sendEnrollmentEmail(newUser);
+              }
+            });
+          }
+        });
+      }
+    }
+  },
+  addAdditionalSeats: function(additionalSeats) {
+    check(additionalSeats, String);
+    let seats = new Future();
+    let user = Meteor.users.findOne(Meteor.userId());
+    if (user) {
+      let organization = Organizations.findOne(user.organizationId);
+      if (organization) {
+        console.log('calling stripe');
+        Stripe.customers.updateSubscription(
+          organization.id,
+          { quantity: additionalSeats },
+          function(error, response) {
+            if (error) {
+              seats.return(error);
+            } else {
+              seats.return(response);
+            }
+          }
+        );
+        return seats.wait();
+      }
     }
   }
 });
