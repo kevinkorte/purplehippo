230d91a3962458408f4df68c812d93a5cf7ae9f7
Kevin Korte <kevinkorte15@gmail.com>
2017-02-25 17:26:52 -0800

    working on updating a client to own organization

--- client/accounts/ui/accountUpdate/accountUpdate.js
+++ client/accounts/ui/accountUpdate/accountUpdate.js
@@ -15,6 +15,24 @@ Template.accountUpdate.events({
       } else {
         let user = Meteor.findUserByEmail(email);
         let profileName = user.profile.name;
+        Meteor.call('stripeCreateCustomer', user.emails[0].address, profileName, function(error, stripeCustomer) {
+          if (error) {
+            console.log(error.reason);
+          } else {
+            let customerId = stripeCustomer.id;
+            let plan = 'basic_0217';
+            Meteor.call('stripeCreateSubscription', customerId, plan, 1, function(error, response) {
+              if (error) {
+                console.log(error.reason);
+              } else {
+                try {
+                  //created new stripe customer and subscription, now need to
+                  //update users account with this information
+                }
+              }
+            })
+          }
+        })
       }
     });
   }
