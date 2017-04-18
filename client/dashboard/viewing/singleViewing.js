2edee1540984e35d55e078d3ee10365bb03cd2a9
kevinkorte <kevinkorte15@gmail.com>
2017-04-17 23:17:35 -0700

    google maps works

---
+++ client/dashboard/viewing/singleViewing.js
@@ -0,0 +1,29 @@
+Template.singleViewing.helpers({
+  viewing: () => {
+    let id = FlowRouter.getParam('id');
+    return Viewings.findOne({_id: id});
+  },
+  viewingMapOptions: function() {
+    let id = FlowRouter.getParam('id');
+    let event = Viewings.findOne({_id: id});
+    if ( GoogleMaps.loaded() && event ) {
+      let eventLat = Number(event.lat);
+      let eventLng = Number(event.lng);
+      return {
+        center: new google.maps.LatLng(eventLat, eventLng),
+        zoom: 17,
+        gestureHandling: 'cooperative'
+      }
+    }
+  }
+});
+
+Template.singleViewing.onRendered(function() {
+  GoogleMaps.ready('viewingMap', function(map) {
+    let marker = new google.maps.Marker({
+      position: map.options.center,
+      map: map.instance,
+      draggable: true,
+    });
+  });
+});
--- server/methods/dashboard/dashboard.js
+++ server/methods/dashboard/dashboard.js
@@ -1,6 +1,10 @@
 Meteor.methods({
-  'addNewViewing'() {
-    let v = Viewings.insert({});
+  'addNewViewing'(address, lat, lng) {
+    check(address, String);
+    check(lat, String);
+    check(lng, String);
+    //Yes, lat and lng are strings
+    let v = Viewings.insert({address: address, lat: lat, lng: lng});
     return v;
   }
 });
